3a0ca2574e4bf8099bb296cf4e7b27d9446d8f57
yusaku0609 <127006537+yusaku0609@users.noreply.github.com>
2023-05-20 18:32:59 +0900

    second commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :profile, presence: true
+  validates :occupation, presence: true
+  validates :position, presence: true
+
+  has_many :prototypes
+  has_many :comments
 end
